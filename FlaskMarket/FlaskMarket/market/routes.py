03c23adb6dec8bfcbd72ba07c477ce86f886084d
AlvisOu <oualvis@gmail.com>
2024-10-26 12:44:34 -0400

    Login and user authentication

--- FlaskMarket/FlaskMarket/market/routes.py
+++ FlaskMarket/FlaskMarket/market/routes.py
@@ -1,7 +1,8 @@
 from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
+from flask_login import login_user
 
 @app.route("/")
 @app.route("/home")
@@ -13,7 +14,7 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route("/register", methods=['GET', 'POST']) # Get for getting the form, POST for posting
+@app.route("/register", methods=['GET', 'POST']) # Get for handling get requests from client, POST for post requests
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -27,3 +28,20 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f"There was an error creating a user: {err_msg}", category='danger')
     return render_template('register.html', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+            ):
+            login_user(attempted_user)
+            flash(f"Success! Yoyu are logged in as: {attempted_user.username}", category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password do not match! Please try again', category='danger')
+
+    return render_template('login.html', form=form)
