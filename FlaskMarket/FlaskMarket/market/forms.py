20fe2018061eece1b68fab69b1c38723b47080ff
AlvisOu <oualvis@gmail.com>
2024-10-26 23:55:06 -0400

    Added more information and purchase modals. Implemented purchasing ability

--- FlaskMarket/FlaskMarket/market/forms.py
+++ FlaskMarket/FlaskMarket/market/forms.py
@@ -28,3 +28,9 @@ class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Sign in')
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Purchase item')
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Sell item')
